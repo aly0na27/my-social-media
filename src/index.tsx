c369a5a7dd60e819270cf230f17f2eb31cbadd6c
aly0na27 <alenakravchenko1910@gmail.com>
2023-12-03 16:39:26 +0300

    WebSocket + reconnect and hook

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,11 @@ import reportWebVitals from './reportWebVitals';
 import ReactDOM from "react-dom/client";
 import {SamuraiJsApp} from "./App";
 import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <DevSupport ComponentPreviews={ComponentPreviews}
-                useInitialHook={useInitial}>
-        <SamuraiJsApp/>
-    </DevSupport>
+     <SamuraiJsApp/>
 );
 
 reportWebVitals();
--- src/pages/ChatPage/ChatPage.tsx
+++ src/pages/ChatPage/ChatPage.tsx
@@ -1,31 +1,49 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import styles from "./ChatPage.module.css"
 import {Col, Row} from "antd";
 import {Form, Formik} from "formik";
 import FormItem from "formik-antd/es/form-item";
 import Input from "formik-antd/es/input"
-import {SendOutlined, SendRounded} from "@mui/icons-material";
+import {SendRounded} from "@mui/icons-material";
 
-const ws = new WebSocket("wss://social-network.samuraijs.com/handlers/ChatHandler.ashx")
+
+type MessageType = {
+    userId: number
+    photo: string
+    userName: string
+    message: string
+}
+
 const ChatPage: React.FC = () => {
 
-    const messages: Array<MessageType> = [
-        {
-            url: "https://ru-static.z-dn.net/files/d96/ced913ba9fe71679ae395a4be5fac683.jpg",
-            name: "Alyona",
-            message: "hi"
-        },
-        {
-            url: "https://ru-static.z-dn.net/files/d96/ced913ba9fe71679ae395a4be5fac683.jpg",
-            name: "Alyona",
-            message: "hi"
-        },
-        {
-            url: "https://ru-static.z-dn.net/files/d96/ced913ba9fe71679ae395a4be5fac683.jpg",
-            name: "Alyona",
-            message: "hi"
-        },
-    ]
+    const [wsChannel, setWsChannel] = useState<null | WebSocket>(null)
+
+    useEffect(() => {
+        let ws: WebSocket
+        const eventHandler = () => {
+            console.log("Error")
+            createChannel()
+        }
+
+        function createChannel() {
+            if (ws) {
+                ws.removeEventListener("close", eventHandler)
+            }
+            ws = new WebSocket("wss://social-network.samuraijs.com/handlers/ChatHandler.ashx")
+            ws.addEventListener("close", eventHandler)
+            setWsChannel(ws)
+
+        }
+        createChannel()
+
+        return () => {
+            ws.removeEventListener("close", eventHandler)
+            ws.close()
+        }
+
+    }, [])
+
+
     return (
         <div>
             <header className={styles.header}>
@@ -34,36 +52,46 @@ const ChatPage: React.FC = () => {
                 </h2>
             </header>
             <main className={styles.main}>
-                <Messages messages={messages}/>
-                <MessageForm/>
+                <Messages wsChannel={wsChannel}/>
+                <MessageForm wsChannel={wsChannel}/>
             </main>
         </div>
     )
 }
 
-type MessageType = {
-    url: string
-    name: string
-    message: string
-}
 
-const Messages: React.FC<{ messages: Array<MessageType> }> = ({messages}) => {
+const Messages: React.FC<{wsChannel: WebSocket | null}> = ({wsChannel}) => {
+
+    const [messages, setMessages] = useState<Array<MessageType>>([])
+
+    useEffect(() => {
+        const eventHandler = (e) => {
+            let newMessages = JSON.parse(e.data);
+            setMessages((prevMessages) => [...prevMessages, ...newMessages])
+        }
+        wsChannel?.addEventListener("message", eventHandler)
+
+        return () => {
+            wsChannel?.removeEventListener("message", eventHandler)
+        }
+    }, [wsChannel])
+
     return (
-        <>
-            {messages.map(m => <Message url={m.url} name={m.name} message={m.message}/>)}
-        </>
+        <div style={{height: "400px", overflowY: "auto"}}>
+            {messages.map(m => <Message userId={m.userId} photo={m.photo} userName={m.userName} message={m.message}/>)}
+        </div>
     )
 }
 
-const Message: React.FC<MessageType> = ({url, name, message}) => {
+const Message: React.FC<MessageType> = ({userId, photo, userName, message}) => {
     return (
         <div>
             <Row>
                 <Col flex={2}>
-                    <img src={url} alt={""} style={{width: "5rem", height: "5rem"}}/>
+                    <img src={photo} alt={""} style={{width: "5rem", height: "5rem"}}/>
                 </Col>
                 <Col flex={3}>
-                    <h4>{name}</h4>
+                    <h4>{userName}</h4>
                     <p>{message}</p>
                 </Col>
             </Row>
@@ -71,20 +99,36 @@ const Message: React.FC<MessageType> = ({url, name, message}) => {
         </div>
     )
 }
-const MessageForm: React.FC = () => {
+const MessageForm: React.FC<{wsChannel: WebSocket | null}> = ({wsChannel}) => {
+
+    const [status, setStatus] = useState<'pending' | 'open'>('pending')
+
+    useEffect(() => {
+        const eventHandler = () => {
+            setStatus('open')
+
+        }
+        wsChannel?.addEventListener('open', eventHandler)
+
+        return () => {
+            wsChannel?.removeEventListener("open", eventHandler)
+        }
+    }, [wsChannel])
     const initialValues = {
         message: ''
     }
     return (
-        <Formik initialValues={initialValues}  onSubmit={values => {
-
+        <Formik initialValues={initialValues}  onSubmit={(values, FormikHelpers) => {
+            wsChannel.send(values.message)
+            // FormikHelpers.setValues(initialValues)
+            FormikHelpers.resetForm()
         }}>
             {props =>
                 <Form>
                     <FormItem name={"message"}>
                         <Input.TextArea name={"message"}/>
                     </FormItem>
-                    <button type={"submit"}>
+                    <button disabled={status !== 'open'} type={"submit"}>
                         <SendRounded style={{transform: "rotate(-45deg)"}}/>
                     </button>
                 </Form>
